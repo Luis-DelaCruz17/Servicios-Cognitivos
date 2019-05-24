5e825dd7076dba5e333fad0e16e15598898216e1
Luis Alfredo De la Cruz Peláez <31258563+Luis-DelaCruz17@users.noreply.github.com>
2019-05-24 15:22:24 -0500

    Add files via upload

---
+++ Azure/face-identify/src/java/com/vg/services/Configuration.java
@@ -0,0 +1,7 @@
+package com.vg.services;
+
+public class Configuration {
+    public static String key = "53a6866d-fb3b-48e1-ade7-d4fb08296ee6";
+    public static String Location = "https://centralus.api.cognitive.microsoft.com/face/v1.0";
+}
+ 
